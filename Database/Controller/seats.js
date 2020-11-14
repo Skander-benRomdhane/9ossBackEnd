885b26d48bc9ce3df360a10134b6bcf4dadb2d7f
omar-chaouachi <omarchaouachi4@gmail.com>
2020-11-14 19:25:47 +0100

    seats

---
+++ Database/Controller/seats.js
@@ -0,0 +1,38 @@
+const mysql = require("mysql");
+const db = require('../Configuration/index.js')
+
+//get all seats
+const getAllSeats = function () {
+    return new Promise((resolve, reject) => {
+      db.connection.query(`SELECT * FROM  seats`, (error, results) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(results);
+      });
+    });
+  };
+
+  //update seat by aviability
+  const updateSeatAvailability = (av) => {
+    let syntax = `UPDATE seats
+  SET availability = '${av.availability}',
+  WHERE id = '${id}'`;
+  
+  return new Promise((resolve, reject) => {
+    db.connection.query(syntax, (error, results) =>{
+      if (error){
+         reject(error);
+      }
+  
+      else{
+         resolve(results);
+      }
+    })
+  })
+  }
+  
+  module.exports = {
+    getAllSeats,
+    updateSeatAvailability
+  };
